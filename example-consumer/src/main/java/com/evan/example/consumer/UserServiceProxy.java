7458c9d3ed1af7af93f0a652476af8ab61b9e59c
Evan <evanparker945@gmail.com>
2024-03-17 21:26:18 +0800

    实现静态代理，服务消费者向服务提供者远程发起调用

--- example-consumer/src/main/java/com/evan/example/consumer/UserServiceProxy.java
+++ example-consumer/src/main/java/com/evan/example/consumer/UserServiceProxy.java
@@ -1,13 +1,43 @@
 package com.evan.example.consumer;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.evan.evanrpc.model.RpcRequest;
+import com.evan.evanrpc.model.RpcResponse;
+import com.evan.evanrpc.serializer.JdkSerializer;
+import com.evan.evanrpc.serializer.Serializer;
 import com.evan.example.commom.model.User;
 import com.evan.example.commom.service.UserService;
+
+import java.io.IOException;
 
 public class UserServiceProxy implements UserService {
 
     @Override
     public User getUser(User user) {
         // 指定序列化器
+        Serializer serializer = new JdkSerializer();
 
+        // 构造并发送请求
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(UserService.class.getName())
+                .methodName("getUser")
+                .args(new Object[]{user})
+                .parameterTypes(new Class[]{User.class})
+                .build();
+        try {
+            byte[] bodyBytes = serializer.serialize(rpcRequest);
+            byte[] result;
+            try(HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+                    .body(bodyBytes)
+                    .execute()) {
+                result = httpResponse.bodyBytes();
+            }
+            RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+            return (User) rpcResponse.getData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }

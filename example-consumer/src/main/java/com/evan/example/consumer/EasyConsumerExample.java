6b03563dbecef34895d21ff87fae5398ed1d7e1a
Evan <evanparker945@gmail.com>
2024-03-17 21:26:36 +0800

    实现静态代理，服务消费者向服务提供者远程发起调用

--- example-consumer/src/main/java/com/evan/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/evan/example/consumer/EasyConsumerExample.java
@@ -5,7 +5,8 @@ import com.evan.example.commom.service.UserService;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
-        UserService userService = null;
+        // 实现代理对象类
+        UserService userService = new UserServiceProxy();
         User user = new User();
         user.setName("evan");
         User newUser = userService.getUser(user);

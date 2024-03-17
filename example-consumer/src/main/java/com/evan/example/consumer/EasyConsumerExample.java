43e0fdaeba87218258d1fd5f5221d4a7cf78e8bb
Evan <evanparker945@gmail.com>
2024-03-17 21:51:03 +0800

    引入了代理工厂

--- example-consumer/src/main/java/com/evan/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/evan/example/consumer/EasyConsumerExample.java
@@ -1,12 +1,13 @@
 package com.evan.example.consumer;
 
+import com.evan.evanrpc.proxy.ServiceProxyFactory;
 import com.evan.example.commom.model.User;
 import com.evan.example.commom.service.UserService;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
         // 实现代理对象类
-        UserService userService = new UserServiceProxy();
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("evan");
         User newUser = userService.getUser(user);
